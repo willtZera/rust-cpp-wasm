eac2ed27b55c694c4165f63efbe2c634c4d1c8f3
IgorKhomenko <khomenkoigor+1@gmail.com>
2024-06-26 16:03:08 +0300

    update build.rs

--- build.rs
+++ build.rs
@@ -1,26 +1,39 @@
 // // export WASI_SDK_PATH=~/Downloads/wasi-sdk-22.0
 // // export WASI_SYSROOT=$WASI_SDK_PATH/share/wasi-sysroot
 
+// The wasi-sdk does not include libstdc++ or libc++ by default. 
+// Instead, it provides the necessary environment for compiling C/C++ code to WebAssembly. 
+// We need to ensure that we are correctly configuring the build to use the C++ standard library available in wasi-sdk.
+//
+//  The cargo:rustc-link-lib=static=c++ and cargo:rustc-link-lib=static=c++abi lines 
+// ensure that the static versions of the C++ standard library and C++ ABI library are linked.
+
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
+    let target = env::var("TARGET").unwrap();
     if target.contains("wasm32") {
-        println!("cargo:rustc-link-search=native={}/share/wasi-sysroot/lib/wasm32-wasi", env!("WASI_SDK_PATH"));
+        let wasi_sdk_path = env::var("WASI_SDK_PATH").expect("WASI_SDK_PATH not set");
+        let sysroot = PathBuf::from(&wasi_sdk_path).join("share/wasi-sysroot");
+        let include_path = sysroot.join("include");
+        let lib_path = sysroot.join("lib/wasm32-wasi");
+
+        println!("cargo:rustc-link-search=native={}", lib_path.display());
         println!("cargo:rustc-link-lib=static=c++");
-        
+        println!("cargo:rustc-link-lib=static=c++abi");
+
         cc::Build::new()
-            // .cpp(true)
-            // .flag("-std=c++17") // Use the appropriate C++ standard
+            // .cpp(true) // this will cause rust-lld: error: unable to find library -lstdc++
             .file("cpp/my_code.cpp")
-            .include(format!("{}/share/wasi-sysroot/include", env!("WASI_SDK_PATH")))
+            .include(include_path)
             .flag("--sysroot")
-            .flag(&format!("{}/share/wasi-sysroot", env!("WASI_SDK_PATH")))
+            .flag(sysroot.to_str().unwrap())
+            .flag("-fno-exceptions") // By adding the -fno-exceptions flag to the cc::Build configuration, you ensure that the C++ code is compiled without exception support, preventing the linker errors related to __cxa_allocate_exception. More info https://github.com/WebAssembly/wasi-sdk?tab=readme-ov-file#notable-limitations
             .compile("my_code");
     } else {
         cc::Build::new()
-            .cpp(true)
+            // .cpp(true)
             .file("cpp/my_code.cpp")
             .compile("my_code");
     }
--- cpp/my_code.cpp
+++ cpp/my_code.cpp
@@ -5,8 +5,9 @@ extern "C"
 {
   void hello()
   {
-    std::string name = "World";
-    // std::cout << "Hello, " << name << " from C++!" << std::endl;
-    printf("Hello from C++ \n");
+    std::string str = "World";
+    const char *cstr = str.c_str();
+    std::cout << "Hello, " << str << " from C++!" << std::endl;
+    printf("Hello from C++, %s \n", cstr);
   }
 }
